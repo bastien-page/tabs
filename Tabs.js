241bc7cbe871771567fe28c897e97bf5c2a213a2
bastien-page <page.bastien.pro@gmail.com>
2023-05-14 18:13:13 +0200

    fix: options class

--- Tabs.js
+++ Tabs.js
@@ -1,5 +1,5 @@
 class Tabs {
-    constructor(elt) {
+    constructor(elt, { ...options }) {
         this.tabs = Array.from(elt.querySelectorAll('[data-tab-item]'))
         this.tabsContent = Array.from(
             elt.querySelectorAll('[data-tab-content]')
@@ -7,6 +7,10 @@ class Tabs {
         this.index = this.tabs.findIndex((elt) =>
             elt.classList.contains('is-active')
         )
+        console.log(options.activeClass)
+        this.activeClass = options.activeClass ?? 'is-active'
+        this.hiddenClass = options.hiddenClass ?? 'd-none'
+        this.showClass = options.showClass ?? 'is-show'
     }
 
     evenHendler() {
@@ -48,28 +52,28 @@ class Tabs {
 
     hiddenTabsContent() {
         this.tabsContent.forEach((tabContent) => {
-            if (tabContent.classList.contains('is-show')) {
-                tabContent.classList.remove('is-show')
-                tabContent.classList.add('d-none')
+            if (tabContent.classList.contains(this.showClass)) {
+                tabContent.classList.remove(this.showClass)
+                tabContent.classList.add(this.hiddenClass)
             }
         })
     }
 
     resetTab() {
         this.tabs.forEach((tab) => {
-            tab.classList.remove('is-active')
+            tab.classList.remove(this.activeClass)
             tab.setAttribute('aria-selected', 'false')
             tab.setAttribute('tabindex', '-1')
         })
     }
 
     showTabContent(tabContent) {
-        tabContent.classList.add('is-show')
-        tabContent.classList.remove('d-none')
+        tabContent.classList.add(this.showClass)
+        tabContent.classList.remove(this.hiddenClass)
     }
 
     activeTab(tab) {
-        tab.classList.add('is-active')
+        tab.classList.add(this.activeClass)
         tab.setAttribute('aria-selected', 'true')
         tab.setAttribute('tabindex', '0')
     }
